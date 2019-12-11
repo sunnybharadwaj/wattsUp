b6c6cb54381881a8637fb8e0503ce578d7bcc685
Sunny Bharadwaj <sunny@chakradesign.co>
2019-12-11 12:42:56 +0530

    Change title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,8 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    WattsUp
+                    WattsUp Energy meter
+
                 </div>
 
                 <div class="links">
